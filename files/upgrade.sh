bda5a10039cdc1b23dbe5a22e9fc90444475cc74
Michael Johansen <michael.johansen@nav.no>
2019-06-11 15:26:06 +0200

    Upgraded version number

--- files/upgrade.sh
+++ files/upgrade.sh
@@ -3,7 +3,6 @@
 # Running this fix first. If things doesn't exists this is really not a problem
 sentry exec /usr/src/sentry/files/fix_migration_error_417.py
 sentry django syncdb
-sentry django migrate
 sentry upgrade --noinput
 sentry exec /usr/src/sentry/files/create_default_projects.py
 sentry exec /usr/src/sentry/files/fix_migration_error_241.py
